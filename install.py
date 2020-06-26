b8a689e8bdf2867c35465c8af437eef25cb64c34
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-06-26 19:28:22 +0530

    Update install.py

--- install.py
+++ install.py
@@ -4,6 +4,7 @@ import time
 print
 os.system("chmod +x *")
 os.system("cd ~/Blackpearl-framework;bash runinstall.sh")
+os.system("cp -r pearl /data/data/com.termux/files/usr/bin/")
 print("Use this only for educational purpose only , we aren't responsible for any other purposes")
 print()
 print("Just type pearl in new terminal session ")
