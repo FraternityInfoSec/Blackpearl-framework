4cc97ead1e06dc71c952d13d75e8221cfe0134b7
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-06-26 17:08:45 +0530

    Update install.py

--- install.py
+++ install.py
@@ -3,8 +3,8 @@ import os
 import time
 print
 os.system("chmod +x *")
-os.system("cd ~/Blackpearl-framework-;./runinstall")
-os.system("cp -r pearl /data/data/com.termux/files/usr/bin")
+os.system("cd ~/Blackpearl-framework;bash runinstall")
+os.system("cp -r pearl /data/data/com.termux/files/usr/bin/")
 os.system("pip install tqdm")
 print( "Use this only for educational purpose only , we aren't responsible for any other purposes")
 print()
