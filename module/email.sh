35c8a809ae10f410d890f523e453db2655f28d37
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-06-26 11:04:23 +0530

    Update email.sh

--- module/email.sh
+++ module/email.sh
@@ -1,1 +1,26 @@
 
+#! /bin/bash 
+
+r='\e[1;31m' 
+
+clear 
+
+read -p $r "DOMAIN :" d
+
+echo 
+
+echo
+
+echo -e $r "HARVESTING...... "
+
+msfconsole -q -x "use auxiliary/gather/search_email_collector; set domain $d; run;"
+
+echo
+
+echo
+
+read -p $r "Press enter to go back" enter
+
+cd Blackpearl-framework/lib
+
+python msf.py
