b5094caf24824ddbf4b434955628e6473c1b7efe
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-06-26 16:46:52 +0530

    Update install.py

--- install.py
+++ install.py
@@ -2,12 +2,15 @@
 import os
 import time
 print
-os.system("cd Blackpearl-framework-;bash runinstall.sh")
-os.system("cp -r pearl /data/data/com.termux/files/usr/bin/")
+os.system("cd Blackpearl-framework-;runinstall")
+os.system("cp -r pearl /data/data/com.termux/files/usr/bin")
 os.system("pip install tqdm")
-print( "Use this only for educational purpose only , we arent responsible for any other purposes")
+print( "Use this only for educational purpose only , we aren't responsible for any other purposes")
+print()
 print("Just type pearl in new terminal session ")
-print(""" FROM 
+print()
+print("""
+FROM 
    ▄████████    ▄████████    ▄████████     ███        ▄████████    ▄████████ ███▄▄▄▄    ▄█      ███     ▄██   ▄   
   ███    ███   ███    ███   ███    ███ ▀█████████▄   ███    ███   ███    ███ ███▀▀▀██▄ ███  ▀█████████▄ ███   ██▄ 
   ███    █▀    ███    ███   ███    ███    ▀███▀▀██   ███    █▀    ███    ███ ███   ███ ███▌    ▀███▀▀██ ███▄▄▄███ 
