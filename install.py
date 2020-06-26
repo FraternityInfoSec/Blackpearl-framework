7402ae7abdc0eb53ce91d3ec711403ef536e1057
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-06-26 19:15:37 +0530

    Update install.py

--- install.py
+++ install.py
@@ -4,17 +4,16 @@ import time
 print
 os.system("chmod +x *")
 os.system("cd ~/Blackpearl-framework;bash runinstall.sh")
+print("Use this only for educational purpose only , we aren't responsible for any other purposes")
+print()
+print("Just type pearl in new terminal session ")
+print()
 os.system("cp -r pearl /data/data/com.termux/files/usr/bin/")
 os.system("chmod +x /data/data/com.termux/files/usr/bin/pearl")
 os.system("chmod +x ~/Blackpearl-framework/lib/hydra.py")
 os.system("chmod +x ~/Blackpearl-framework/lib/msf.py")
 os.system("chmod +x ~/Blackpearl-framework/lib/sql.py")
 os.system("chmod +x ~/Blackpearl-framework/lib/nmapper.sh")
-os.system("pip install tqdm")
-print()
-print("Use this only for educational purpose only , we aren't responsible for any other purposes")
-print()
-print("Just type pearl in new terminal session ")
 print()
 print("""
 FROM 
