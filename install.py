3f227bc00d07ccf12a6a59acbf72897100e70d48
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-06-26 12:41:00 +0530

    Create install.py

---
+++ install.py
@@ -0,0 +1,9 @@
+#!/bin/env python
+import os
+import time
+print
+os.system("cd Blackpearl-framework;bash runinstall.sh")
+os.system("cp -r pearl /data/data/com.termux/files/usr/bin/")
+os.system("pip install tqdm")
+print( "Use this only for educational purpose only , we arent responsible for any other purposes")
+print("Just type pearl in new terminal session ")
