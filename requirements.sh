9b3bd1e3a422a753b60e3165d60f6a2b9996fbcd
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-06-26 19:19:27 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -9,6 +9,13 @@ sleep 2
 pkg upgrade
 pkg update
 sleep 2
+cp -r pearl /data/data/com.termux/files/usr/bin/
+chmod +x /data/data/com.termux/files/usr/bin/pearl
+chmod +x ~/Blackpearl-framework/lib/hydra.py
+chmod +x ~/Blackpearl-framework/lib/msf.py
+chmod +x ~/Blackpearl-framework/lib/sql.py
+chmod +x ~/Blackpearl-framework/lib/nmapper.sh
+print()
 pip install requests
 cp -r pearl /data/data/com.termux/files/usr/bin
 echo
