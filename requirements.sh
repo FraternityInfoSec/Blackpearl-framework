b1eff2c96762ab643acffdd03f458940e088e8d0
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-06-26 19:21:31 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -15,7 +15,6 @@ chmod +x ~/Blackpearl-framework/lib/hydra.py
 chmod +x ~/Blackpearl-framework/lib/msf.py
 chmod +x ~/Blackpearl-framework/lib/sql.py
 chmod +x ~/Blackpearl-framework/lib/nmapper.sh
-print()
 pip install requests
 cp -r pearl /data/data/com.termux/files/usr/bin
 echo
