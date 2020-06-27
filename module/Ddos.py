91e3a02605b307bf2493a2a7d6be2e687b8bc938
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-06-27 11:42:50 +0530

    Update Ddos.py

--- module/Ddos.py
+++ module/Ddos.py
@@ -18,12 +18,7 @@ bytes = random._urandom(1490)
 #############
 
 os.system("clear")
-os.system("figlet DDos Attack")
-print
-print "Author   : CIPHER"
-
-print "github   : https://github.com/ProfessorCipher"
-
+os.system("figlet W3BJ4M")
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
