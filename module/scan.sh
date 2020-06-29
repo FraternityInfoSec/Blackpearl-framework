8a432ab4eeef2ccb7becc4614dd65ca774cdfa42
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-06-29 13:44:37 +0530

    Update scan.sh

--- module/scan.sh
+++ module/scan.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 read -p "Enter website : " w
 echo -e "Starting scanning"
-python sqlmap.py -u "$w""
+python sqlmap.py -u '$w'"
+
 read -p "Press enter to go back" enter
 cd Blackpearl-framework/lib
 python sql.py
