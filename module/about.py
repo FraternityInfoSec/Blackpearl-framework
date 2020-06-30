60ca74d2e6dbf9a8153ae3b0042881854cde9e75
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-06-30 13:31:23 +0530

    Update about.py

--- module/about.py
+++ module/about.py
@@ -1,5 +1,6 @@
 import time
 import sys
+from pearl import *
 G = '\033[92m'
 def delay_print(s):
     for c in s:
@@ -8,3 +9,5 @@ def delay_print(s):
         time.sleep(0.20)
 
 delay_print(G + "Hi all.This is just a project from the Fraternity. This for educational purposes only. We are not responsible for any illegal activities.For any issues or errors contact us on our email Cipherweb777@gmail.com @gmail.com") 
+print()
+main()
