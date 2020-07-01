20fab8929c576a177dc681fe8baaf2011e25dc89
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-07-01 11:19:26 +0530

    Delete table.sh

--- module/table.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-read -p "Enter website :" w
-read -p "Enter database :" d
-python sqlmap.py -u "$w" --tables -D $d
-read -p "Press enter to go back" enter
-cd Blackpearl-framework/lib
-python sql.py
