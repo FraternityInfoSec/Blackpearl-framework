ff66cac351e81ed0205d24d7e8bb94f0a0dd952b
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-07-01 11:18:04 +0530

    Delete db.sh

--- module/db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-read -p "Enter the website :" w
-echo -e "Starting attack" 
-python sqlmap.py -u "$w" --dbs
-read -p "Press enter to go back" enter
-cd Blackpearl-framework/lib
-python sql.py
