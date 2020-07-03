c09a57b4fbd9fe82c4e9fbe4b5ba28e792bb0515
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-07-03 15:49:26 +0530

    Update install.py

--- install.py
+++ install.py
@@ -11,13 +11,14 @@ os.system("cp -r pearl /data/data/com.termux/files/usr/bin/")
 os.system("cp -r ~/Blackpearl-framework/run  /data/data/com.termux/files/usr/bin/")
 print()
 print("BLACKPEARL FRAMEWORK INSTALLED SUCCESSFULLY")
-print
+print() 
 print("Use this only for educational purpose only , we aren't responsible for any other purposes")
 print()
+os.system("cd $HOME") 
 print("Just type pearl in new terminal session ")
 print()
 print("""
-FROM 
+FROM THE
  ___  __       ___  ___  __         ___     
 |__  |__)  /\   |  |__  |__) |\ | |  |  \ / 
 |    |  \ /~~\  |  |___ |  \ | \| |  |   |                                             
