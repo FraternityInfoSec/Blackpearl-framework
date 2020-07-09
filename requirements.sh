c7fb8410ba11c8ae08f5505c64fadd1903c303b2
Cipher <62390774+ProfessorCipher@users.noreply.github.com>
2020-07-09 11:34:55 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -18,10 +18,10 @@ cp -r ~/Blackpearl-framework/pass2.txt ~/Blackpearl-framework/lib/
 cp -r ~/Blackpearl-framework/pass3.txt ~/Blackpearl-framework/lib/
 cp -r pearl /data/data/com.termux/files/usr/bin/
 chmod +x /data/data/com.termux/files/usr/bin/pearl
-chmod +x ~/Blackpearl-framework/lib/hydra.py
-chmod +x ~/Blackpearl-framework/lib/msf.py
-chmod +x ~/Blackpearl-framework/lib/sql.py
-chmod +x ~/Blackpearl-framework/lib/nmapper.sh
+
+
+
+
 pip install requests
 echo
 echo "Requirements Satisfied"
