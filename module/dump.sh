f629f6befd39a21126fc276b66cdc6d75c8ddc6b
Cipher <62390774+FraternityInfoSec@users.noreply.github.com>
2020-09-24 15:49:45 +0530

    Delete dump.sh

--- module/dump.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-read -p "Enter website :" w
-read -p "Enter database :" d
-read -p "Enter table :" t
-echo -e "Dumping data" 
-python sqlmap.py -u "$w" --dump -D $d -T $t
-read -p "Press enter to go back" enter
-python sql.py
