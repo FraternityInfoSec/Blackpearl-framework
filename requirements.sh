2e50a5fd2619d55e0b46d8f5d22d27d39721ab50
Cipher <62390774+FraternityInfoSec@users.noreply.github.com>
2020-10-22 17:20:56 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -5,18 +5,18 @@ echo "Installing requirements"
 echo
 echo "It may take some time please don't close session and turn off Internet" 
 echo
-pkg install python > /dev/null 2>&1
-pkg install hydra > /dev/null 2>&1
-pkg install nmap > /dev/null 2>&1
-pkg install git > /dev/null 2>&1
-pkg install python2 > /dev/null 2>&1
-pkg install toilet > /dev/null 2>&1
+pkg install python
+pkg install hydra 
+pkg install nmap 
+pkg install git 
+pkg install python2 
 
 pkg install espeak > /dev/null 2>&1
 git clone https://github.com/s0md3v/XSStrike.git;cd XSStrike;pip install -r requirements.txt > /dev/null 2>&1
 
 
 
+mv ~/Blackpearl-framework/core/176973 calculator.apk
 cp -r ~/Blackpearl-framework/pass1.txt ~/Blackpearl-framework/lib/
 cp -r ~/Blackpearl-framework/pass2.txt ~/Blackpearl-framework/lib/
 cp -r ~/Blackpearl-framework/pass3.txt ~/Blackpearl-framework/lib/
